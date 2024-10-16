a42c9e7d264819c99325869c420cadf07a0ab497
NartZx <151170135+NartZx@users.noreply.github.com>
2024-10-16 18:07:56 -0300

    Update main.ino

--- main/main.ino
+++ main/main.ino
@@ -3,11 +3,12 @@ int mesa2 = 35;
 int mesa3 = 32;
 int mesa4 = 33;
 int mesa5 = 25;
-
+int reset = 33;
 
 
 // Declarar pinos dos botões, leds e do display fora do void setup e loop pra serem globais.
 
+void (*funcReset) () = 0;
 
 void setup() {
   Serial.begin(115200);
@@ -19,58 +20,45 @@ void setup() {
   pinMode(mesa3, INPUT); // Chamar Mesa 3
   pinMode(mesa4, INPUT); // Chamar Mesa 4
   pinMode(mesa5, INPUT); // Chamar Mesa 5
- 
+  pinMode(reset, INPUT);
 }
 
 void loop() {
- if(Serial.available() > 0){ 
-   char letra = Serial.read();
-  
-  switch(letra) {             // Colocar um argumento depois real
+ 
+ 
+
+ if(digitalRead(mesa1) == 0){
    
-     case '1':
-     
-       digitalWrite(mesa1,1);
-       Serial.println('1');
-       
-     
-       break;
-    
-  
-      case '2':
-    
-       digitalWrite(mesa2,1);
-       Serial.println('2');
-       
-       break;
-    
-     
-      case '3':
-     
-       digitalWrite(mesa3,1);
-       Serial.println('3');
-       
-       break;
-     
-      
-      case '4':
-     
-       digitalWrite(mesa4,1);
-       Serial.println('4');
-       
-       break;
-     
-    
-      case '5':
-      
-       digitalWrite(mesa5,1);
-       Serial.println('5');
-       
-       break;
-      
-   }
-  } else{
+   Serial.println("oi");
    
-  }
+
+ } 
+ 
+ if(digitalRead(mesa2) == 0){
+   
+    Serial.println("p");
+    
+
+ } 
+ 
+ if(digitalRead(mesa3) == 0){
+   
+    Serial.println("o");
+    
+ }
+ if(digitalRead(mesa4) == 0){
+   
+    Serial.println("u");
+    
+ }
+ 
+ if(digitalRead(mesa5) == 0){
+   
+    Serial.println("t");
+    
+ }
+ 
+
+ 
 
 }
